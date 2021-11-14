2c60f67f338098ee0a2050975f16ebbad9211610
nleclaire <noah.leclaire@gmail.com>
2021-11-14 02:08:27 -0500

    adds run + split model

---
+++ run_stats/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RunStatsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'run_stats'
---
+++ run_stats/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Run(models.Model):
+    """A run the user wants to track."""
+    date = models.DateTimeField()
+    time = models.TimeField()
+    distance = models.DecimalField(decimal_places=2)
+    average_speed = models.DecimalField(decimal_places=1)
+    steps = models.IntegerField()
+    calories_burned = models.IntegerField()
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.distance + " miles in " self.time " minutes"
+
+class Split(models.Model):
+    """A time split for each mile on the run."""
+    date = models.DateTimeField()
+    time = models.TimeField()
+    full_mile = models.BooleanField()
+    run = models.ForeignKey(Run, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.time
