572cb7429df89822557cd0f86d17b3e3c4f2276e
nleclaire <noah.leclaire@gmail.com>
2021-11-15 01:20:29 -0500

    adds superuser, registers models w/ admin site, fixes models

--- run_stats/admin.py
+++ run_stats/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Run, Split
 
-# Register your models here.
+admin.site.register(Run)
+admin.site.register(Split)
--- run_stats/models.py
+++ run_stats/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 class Run(models.Model):
     """A run the user wants to track."""
-    date = models.DateTimeField()
+    date = models.DateField()
     time = models.TimeField()
-    distance = models.DecimalField(max_digits=3, decimal_places=2)
+    distance = models.DecimalField(max_digits=4, decimal_places=2)
     average_speed = models.DecimalField(max_digits=3, decimal_places=1)
-    steps = models.IntegerField()
     calories_burned = models.IntegerField()
+    steps = models.IntegerField()
 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.distance + " miles in " + self.time + " minutes"
+        return str(self.distance) + " miles - " + str(self.time)[3:] + " minutes"
 
 class Split(models.Model):
     """A time split for each mile on the run."""
     date = models.DateTimeField()
     time = models.TimeField()
-    full_mile = models.BooleanField()
+    length = models.DecimalField(max_digits=3, decimal_places=2)
     run = models.ForeignKey(Run, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.time
+        return str(self.time)[3:]
